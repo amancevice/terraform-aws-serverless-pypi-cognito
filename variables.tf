445c13ae9a167c732e7b18cdac22f2a88e035d96
Alexander Mancevice <amancevice@cargometrics.com>
2020-02-19 18:03:00 -0500

    Revert "remove trailing periods"
    This reverts commit 8243833c2a0d85f0516a428ec941c33376c9d981.

--- variables.tf
+++ variables.tf
@@ -1,46 +1,46 @@
 variable api_id {
-  description = "API Gateway REST API ID"
+  description = "API Gateway REST API ID."
 }
 
 variable authorizer_name {
-  description = "API Gateway authorizer name"
-  default     = "COGNIT"
+  description = "API Gateway authorizer name."
+  default     = "COGNITO"
 }
 
 variable lambda_description {
-  description = "REST API authorizer Lambda function description"
-  default     = "PyPI REST API Authorize"
+  description = "REST API authorizer Lambda function description."
+  default     = "PyPI REST API Authorizer"
 }
 
 variable lambda_function_name {
-  description = "REST API authorizer Lambda function name"
+  description = "REST API authorizer Lambda function name."
 }
 
 variable log_group_retention_in_days {
-  description = "CloudWatch log group retention period"
+  description = "CloudWatch log group retention period."
   default     = 30
 }
 
 variable policy_name {
-  description = "IAM role inline policy name"
-  default     = "pypi-authorizer-permission"
+  description = "IAM role inline policy name."
+  default     = "pypi-authorizer-permissions"
 }
 
 variable role_description {
-  description = "Lambda functions IAM role description"
-  default     = "PyPI Lambda permission"
+  description = "Lambda functions IAM role description."
+  default     = "PyPI Lambda permissions"
 }
 
 variable role_name {
-  description = "Authorizer Lambda function role name"
+  description = "Authorizer Lambda function role name."
 }
 
 variable tags {
-  description = "Resource tags"
+  description = "Resource tags."
   type        = map
   default     = {}
 }
 
 variable user_pool_name {
-  description = "Cognito user pool name"
+  description = "Cognito user pool name."
 }
